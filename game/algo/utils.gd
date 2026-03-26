95f17eb65c9e277a1f0f9e7d2272f7d7265ebef9
Pavel Barabanov <barabanovpv95@gmail.com>
2026-03-26 23:06:11 +0300

    feat: add true screen wrap for shi

--- game/algo/utils.gd
+++ game/algo/utils.gd
@@ -1,8 +1,9 @@
 class_name Utils
 extends Node
 
-static func is_any_point_on_screen(node: Node2D, points: PackedVector2Array) -> bool:
-	var screen_size := node.get_viewport_rect().size
+static func is_any_point_on_screen(node: Node2D, shape_obj: Variant) -> bool:
+	var screen_size := ScreenGlobal.screen_size
+	var points: PackedVector2Array = _dispatch_to_points(shape_obj)
 	for p in points:
 		var point := node.to_global(p)
 		if (point.x > 0 and point.x < screen_size.x and
@@ -11,8 +12,20 @@ static func is_any_point_on_screen(node: Node2D, points: PackedVector2Array) -> 
 	return false
 
 
-static func is_any_point_off_screen(node: Node2D, points: PackedVector2Array) -> bool:
-	var screen_size := node.get_viewport_rect().size
+static func is_all_points_on_screen(node: Node2D, shape_obj: Variant) -> bool:
+	var screen_size := ScreenGlobal.screen_size
+	var points: PackedVector2Array = _dispatch_to_points(shape_obj)
+	for p in points:
+		var point := node.to_global(p)
+		if not (point.x > 0 and point.x < screen_size.x and
+			point.y > 0 and point.y < screen_size.y ):
+			return false
+	return true
+
+
+static func is_any_point_off_screen(node: Node2D, shape_obj: Variant) -> bool:
+	var screen_size := ScreenGlobal.screen_size
+	var points: PackedVector2Array = _dispatch_to_points(shape_obj)
 	for p in points:
 		var point := node.to_global(p)
 		if not (
@@ -22,8 +35,29 @@ static func is_any_point_off_screen(node: Node2D, points: PackedVector2Array) ->
 	return false
 
 
-static func is_node_on_screen_edge(node: Node2D, points: PackedVector2Array) -> bool:
+static func is_all_points_off_screen(node: Node2D, shape_obj: Variant) -> bool:
+	return not is_all_points_on_screen(node, shape_obj)
+
+
+static func is_node_on_screen_edge(node: Node2D, shape_obj: Variant) -> bool:
 	return (
-		is_any_point_on_screen(node, points) and
-		is_any_point_off_screen(node, points)
+		is_any_point_on_screen(node, shape_obj) and
+		is_any_point_off_screen(node, shape_obj)
 	)
+
+
+static func _dispatch_to_points(shape_obj: Variant) -> PackedVector2Array:
+	var res: PackedVector2Array
+	if shape_obj is Polygon2D:
+		res = shape_obj.polygon
+	if shape_obj is CollisionShape2D:
+		var shape_rect: Rect2 = shape_obj.shape.get_rect()
+		res = [
+			shape_rect.position,
+			shape_rect.position + Vector2(shape_rect.size.x, 0),
+			shape_rect.position + Vector2(0, shape_rect.size.y),
+			shape_rect.end,
+		]
+	else:
+		assert(false, "Unknown type of shape")
+	return res
--- game/ship.gd
+++ game/ship.gd
@@ -1,14 +1,20 @@
 class_name Ship
 extends Node2D
 
+enum GhostLocation {
+	TOP,
+	DOWN,
+	LEFT,
+	RIGHT,
+}
+
 @export var angualr_speed := 2 * PI
 @export var speed := 400
 @export var bullet_scene: PackedScene
 
-@onready var bullet_marker: Marker2D = %BulletMarker
 @onready var ship: = %ShipPlayer
 
-var _ghosts: Array[CharacterBody2D]
+var _ghosts: Dictionary[GhostLocation, Node2D]
 
 
 func _ready() -> void:
@@ -34,41 +40,72 @@ func _process(_delta: float) -> void:
 	ship.velocity = velocity
 	ship.move_and_slide()
 
-	for ghost in _ghosts:
+	for ghost: Node2D in _ghosts.values():
 		ghost.rotation = ship.rotation
-		ghost.velocity = ship.velocity
-		ghost.move_and_slide()
+
+	_ghosts[GhostLocation.TOP].position = ship.position - Vector2(0, ScreenGlobal.screen_size.y)
+	_ghosts[GhostLocation.DOWN].position = ship.position + Vector2(0, ScreenGlobal.screen_size.y)
+	_ghosts[GhostLocation.RIGHT].position = ship.position + Vector2(ScreenGlobal.screen_size.x, 0)
+	_ghosts[GhostLocation.LEFT].position = ship.position - Vector2(ScreenGlobal.screen_size.x, 0)
+
+	if is_all_points_off_screen(ship):
+		for index: GhostLocation in GhostLocation.values():
+			if is_all_points_on_screen(_ghosts[index]):
+				var ghost_to_swap: Node2D
+				ghost_to_swap = _ghosts[index]
+				_ghosts[index] = ship
+				ship = ghost_to_swap
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	var bullet: Bullet
+	var bullet_marker: Marker2D = ship.get_node("BulletMarker")
+
 	if event.is_action_pressed("fire"):
 		bullet = bullet_scene.instantiate()
 		add_child(bullet)
 		bullet.global_position = bullet_marker.global_position
 		bullet.rotation = ship.rotation
 
-		for ghost in _ghosts:
-			bullet = bullet_scene.instantiate()
-			var marker: Marker2D = ghost.get_node("BulletMarker")
-			add_child(bullet)
-			bullet.global_position = marker.global_position
-			bullet.rotation = ship.rotation
-
-
-func add_ghosts() -> void:
-	_add_ghost(Vector2(global_position.x, global_position.y + ScreenGlobal.screen_size.y))
-	_add_ghost(Vector2(global_position.x, global_position.y - ScreenGlobal.screen_size.y))
-	_add_ghost(Vector2(global_position.x + ScreenGlobal.screen_size.x, global_position.y))
-	_add_ghost(Vector2(global_position.x - ScreenGlobal.screen_size.x, global_position.y))
-
-
-func _add_ghost(pos: Vector2) -> void:
-	var ghost: CharacterBody2D = ship.duplicate()
-	add_child(ghost)
-	ghost.global_position = pos
-	_ghosts.append(ghost)
+# TODO: turn this in component
 
 
 func destroy() -> void:
 	queue_free()
+
+#region Screen Wrap
+# Should implements this functions in order
+# to have a "true" screen wrap
+func add_ghosts() -> void:
+	add_ghost(GhostLocation.DOWN, Vector2(global_position.x, global_position.y + ScreenGlobal.screen_size.y))
+	add_ghost(GhostLocation.TOP, Vector2(global_position.x, global_position.y - ScreenGlobal.screen_size.y))
+	add_ghost(GhostLocation.RIGHT, Vector2(global_position.x + ScreenGlobal.screen_size.x, global_position.y))
+	add_ghost(GhostLocation.LEFT, Vector2(global_position.x - ScreenGlobal.screen_size.x, global_position.y))
+
+
+func add_ghost(location: GhostLocation, pos: Vector2) -> void:
+	var ghost: CharacterBody2D = ship.duplicate()
+	add_child(ghost)
+	ghost.global_position = pos
+	_ghosts[location] = ghost
+
+
+func rebuild_ghosts_positions() -> void:
+	pass
+
+
+func is_all_points_off_screen(node: Node2D) -> bool:
+	var collision_shape: CollisionShape2D = node.get_node("HitboxComponent/CollisionShape2D")
+	return Utils.is_all_points_off_screen(
+		node,
+		collision_shape,
+	)
+
+
+func is_all_points_on_screen(node: Node2D) -> bool:
+	var collision_shape: CollisionShape2D = node.get_node("HitboxComponent/CollisionShape2D")
+	return Utils.is_all_points_on_screen(
+		node,
+		collision_shape,
+	)
+#endregion
