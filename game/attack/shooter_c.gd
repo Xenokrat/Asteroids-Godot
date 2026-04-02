65b18927fd5c962f6a851ad473503a370713c990
Pavel Barabanov <barabanovpv95@gmail.com>
2026-04-02 09:55:53 +0300

    shooter component

--- game/attack/shooter_c.gd
+++ game/attack/shooter_c.gd
@@ -1,17 +1,13 @@
 class_name Shooter_C
 extends Node2D
 
-@export var root: Node2D
+@export var bullet_space: Node2D
+@export var shooting_point: Marker2D
 @export var bullet_scene: PackedScene
-
-
-func _ready() -> void:
-	assert(root.has_method("get_shooter_marker"), "Sooter_C root node should implement `get_shooter_marker`")
 
 
 func shoot() -> void:
 	var bullet: Bullet = bullet_scene.instantiate()
-	root.add_child(bullet)
-	var marker_node: Marker2D = root.get_shooter_marker()
-	print(marker_node)
-	bullet.global_position = marker_node.global_position
+	bullet_space.add_child(bullet)
+	bullet.global_position = shooting_point.global_position
+	bullet.global_rotation = shooting_point.global_rotation
